feeabc098d1bc15255d2a685a689fc88b9fd9989
Stacy Daniel <stacydn23@gmail.com>
2020-09-20 18:13:10 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+var doWhileLoop(interger);{
+
+}
+function forLoop(array){;
+  for (let 1 = 0; 1 < 25; 1++){
+    if (1==1){
+      array.push("I am 1 stranger loop."){
+  }
+  else{
+    array.push ("I am ${1} strange loop$")
+  }
+
+        
